3e6d8fddee2d1e6be7af5e0f0c164aba81d6e297
blackle <isabelle@blackle-mori.com>
2019-09-18 01:56:23 -0400

    Make benchmark multithreaded

--- benchmark.c
+++ benchmark.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include "basilisk.h"
+
+#define POOL_SIZE 8
+
+void basilisk_stress(int count) {
+	basilisk_ctx basilisk;
+	basilisk_init(&basilisk, 0);
+
+	for (int i = 0; i < count; i++) {
+		basilisk_step(&basilisk);
+	}
+}
 
 int main(int argc, char** argv) {
 	if (argc != 2) {
@@ -8,11 +23,16 @@ int main(int argc, char** argv) {
 	}
 	int count = atoi(argv[1]);
 
-	basilisk_ctx basilisk;
-	basilisk_init(&basilisk, 0);
-
-	for (int i = 0; i < count; i++) {
-		basilisk_step(&basilisk);
+	for (int i = 0; i < POOL_SIZE; i++) {
+		if (fork() == 0) {
+			prctl(PR_SET_PDEATHSIG, SIGHUP);
+			basilisk_stress(count);
+			return 0;
+		}
 	}
+
+	int status = 0;
+	while (wait(&status) > 0);
+
 	return 0;
 }
